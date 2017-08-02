d1c8568cf276930601df55352ea7c25beca048a9
Igor Macedo <igormacedo94@yahoo.com.br>
2017-08-02 19:49:20 -0300

    page navigation done

--- todoapp/todoapp/Page1.xaml.cs
+++ todoapp/todoapp/Page1.xaml.cs
@@ -3,21 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace todoapp
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
     {
-        public MainPage()
+        public Page1()
         {
             InitializeComponent();
         }
 
         private void Button_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Alerta!", "Cuidado", "#foratemer!");
-            Navigation.PushAsync(new Page1());
+            Navigation.PushAsync(new MainPage());
         }
     }
 }
